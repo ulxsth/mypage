e880de2cb6f2cbcc53b22967fd7590b9575ffc42
ulxsth <ulxsth@gmail.com>
2026-03-09 00:48:53 +0900

    feat: enable remark-breaks for simple newline in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkBreaks from 'remark-breaks';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkBreaks],
+	},
 });
